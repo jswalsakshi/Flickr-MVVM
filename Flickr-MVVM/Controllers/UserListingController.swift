cc3ad645f78bc597e9324a5c766840bb9482f5cf
sakshi.jaiswal <sakshi.jaiswal@actolap.com>
2020-07-18 20:44:56 +0530

    StoryBoard Design user list

--- Flickr-MVVM/Controllers/UserListingController.swift
+++ Flickr-MVVM/Controllers/UserListingController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  UserListingController.swift
 //  Flickr-MVVM
 //
 //  Created by Sakshi Jaiswal on 18/07/20.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class UserListingController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
-
